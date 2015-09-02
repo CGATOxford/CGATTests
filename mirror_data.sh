812942d48428243e153e0ed4d60fdfb2abfbd5bc
Andreas Heger <andreas.heger@gmail.com>
2015-09-02 10:09:06 +0100

    use genome user

--- mirror_data.sh
+++ mirror_data.sh
@@ -4,8 +4,8 @@
 
 # Mysql tables for test_ancestral_repeats
 echo "import repeats from hg19 - single large table"
-# mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction hg19 rmsk | mysql -h gandalf -u andreas hg19
+mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction hg19 rmsk | mysql -h gandalf -u genome hg19
 echo "import repeats from mm9 - multiple tables"
 for x in `mysql -h genome-mysql.cse.ucsc.edu -u genome -A -e "show tables like '%rmsk'" mm9 -B -N`; do
-	mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction mm9 $x | mysql -h gandalf -u andreas mm9;
+	mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction mm9 $x | mysql -h gandalf -u genome mm9;
 done
