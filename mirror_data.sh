4d85e9e61dab071dc378749aa2c4a4de10781a53
Andreas Heger <andreas.heger@gmail.com>
2015-09-03 13:32:00 +0100

    change order of targets in test_annotations to build summary after other targets

--- mirror_data.sh
+++ mirror_data.sh
@@ -4,8 +4,12 @@
 
 # Mysql tables for test_ancestral_repeats
 echo "import repeats from hg19 - single large table"
-mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction hg19 rmsk | mysql -h gandalf -u genome hg19
+#mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction hg19 rmsk | mysql -h gandalf -u genome hg19
 echo "import repeats from mm9 - multiple tables"
-for x in `mysql -h genome-mysql.cse.ucsc.edu -u genome -A -e "show tables like '%rmsk'" mm9 -B -N`; do
-	mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction mm9 $x | mysql -h gandalf -u genome mm9;
-done
+#for x in `mysql -h genome-mysql.cse.ucsc.edu -u genome -A -e "show tables like '%rmsk'" mm9 -B -N`; do
+#	mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction mm9 $x | mysql -h gandalf -u genome mm9;
+#done
+
+# Mysql tables for annotations pipeline
+echo "import cpgislands from hg19"
+mysqldump -h genome-mysql.cse.ucsc.edu -u genome --single-transaction hg19 cpgIslandExt | mysql -h gandalf -u genome hg19
