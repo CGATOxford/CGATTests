65e0ec4f3e2bbca6ac2b3780165c2cbfaf6cad84
Andreas Heger <andreas.heger@gmail.com>
2015-09-29 14:24:19 +0100

    use py.test

--- jenkins_cgat_codecollection.sh
+++ jenkins_cgat_codecollection.sh
@@ -9,10 +9,8 @@ export SGE_CELL=default
 export MODULEPATH=/usr/share/Modules/modulefiles:/etc/modulefiles:/ifs/apps/modulefiles
 eval `modulecmd bash load apps/java apps/python apps/perl apps/graphlib bio/alignlib bio/all apps/emacs`
 
-TEST_IMPORT=1
-TEST_STYLE=1
-TEST_CMDLINE=1
-TEST_SCRIPTS=1
+# number of parallel jobs to run for testing
+NUM_JOBS=4
 
 # enter working directory. Needs to be on /ifs and mounted everywhere
 # /ifs/projects not possible as jenkins not part of projects group.
@@ -43,21 +41,8 @@ cd cgat
 git pull
 python setup.py develop
 
-# run nosetests
+# run tests
 cd $workdir/cgat
 
-if [ $TEST_IMPORT ] ; then
-    nosetests -v tests/test_import.py ;
-fi
+py.test -n $NUM_JOBS tests/test_*.py
 
-if [ $TEST_STYLE ] ; then
-    nosetests -v tests/test_style.py ;
-fi
-
-if [ $TEST_CMDLINE ] ; then
-    nosetests -v tests/test_commandline.py ;
-fi
-
-if [ $TEST_SCRIPTS  ] ; then
-    nosetests -v tests/test_scripts.py ;
-fi
