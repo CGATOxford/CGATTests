69a635880463499d69bde5a1f80fb5079416e9dc
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-02-23 14:41:23 +0000

    update jenkins testing scripts

--- jenkins_cgat_code.sh
+++ jenkins_cgat_code.sh
@@ -1,76 +1,22 @@
 #!/bin/bash -xe
 
-export DRMAA_LIBRARY_PATH=/ifs/apps/system/sge-6.2/lib/lx24-amd64/libdrmaa.so
-export SGE_ROOT=/ifs/apps/system/sge-6.2
-export SGE_CLUSTER_NAME=cgat
-export SGE_ARCH=lx24_x86
-export SGE_CELL=default
-
 if [ -z "$JENKINS_PYTHON_VERSION" ] ; then
     JENKINS_PYTHON_VERSION="2.7"
 fi
 
-if [[ $JENKINS_PYTHON_VERSION == "2.7" ]] ; then
-    PYTHON_MODULES="apps/python";
-    PYTHON_EXECUTABLE="python2.7";
-elif [[ $JENKINS_PYTHON_VERSION == "3.5" ]] ; then
-    PYTHON_MODULES="apps/python3";
-    PYTHON_EXECUTABLE="python3.5";
-else
+if [ $JENKINS_PYTHON_VERSION != "2.7" ] && [ $JENKINS_PYTHON_VERSION != "3.5" ] ; then
     echo "unsupported python version ${JENKINS_PYTHON_VERSION}"
     exit 1
 fi
 
-export MODULEPATH=/usr/share/Modules/modulefiles:/etc/modulefiles:/ifs/apps/modulefiles
-eval `modulecmd bash load ${PYTHON_MODULES} bio/all`
+# export env variables to installation script
+export JENKINS_PYTHON_VERSION
+export TEST_ALL=1
 
-# number of parallel jobs to run for testing
-NUM_JOBS=4
+# Required when testing install-CGAT-tools.sh on a branch called SLV-install-jenkins
+wget -O install-CGAT-scripts.sh https://raw.githubusercontent.com/CGATOxford/cgat/SLV-install-jenkins/install-CGAT-tools.sh
+bash install-CGAT-scripts.sh --jenkins
 
-# enter working directory. Needs to be on /ifs and mounted everywhere
-# /ifs/projects not possible as jenkins not part of projects group.
-WORKDIR=/ifs/mirror/jenkins/${JOB_NAME}
+# install and test cgat scripts
+# bash install-CGAT-tools.sh --jenkins
 
-if [ ! -d ${WORKDIR} ]; then
-    mkdir -p ${WORKDIR}
-fi
-
-cd ${WORKDIR}
-# setup virtual environment
-rm -rf test_python
-PYTHON_EXEC=`/usr/bin/which ${PYTHON_EXECUTABLE}`
-virtualenv --python=${PYTHON_EXEC} --system-site-packages test_python
-
-# activate virtual environment
-source test_python/bin/activate
-
-cd ${WORKSPACE}
-python setup.py develop
-
-PYTHON=`which python`
-NOSE=`which nosetests`
-
-# some debugging information
-echo "----------------------------------------------"
-printenv
-which python
-python -c 'import numpy; print numpy.version.version'
-python -c 'import pysam; print pysam.__version__'
-python -c 'import matplotlib; print matplotlib.__version__'
-python -c 'from matplotlib.externals.six.moves.urllib.parse import quote'
-echo "python is $PYTHON"
-echo "nose is $NOSE"
-echo "----------------------------------------------"
-
-# run tests
-cd ${WORKSPACE}
-echo -e "restrict:\n    manifest:\n" > tests/_test_commandline.yaml
-# Issues with py.test and CGAT paths
-# py.test -n ${NUM_JOBS} tests/test_*.py
-# nosetests --processes ${NUM_JOBS} tests/test_*.py
-# Running all tests with multiple processes fails, there seem to
-# issues with what libraries are being picked up (VE problems?)
-$PYTHON $NOSE -v tests/test_import.py
-$PYTHON $NOSE -v tests/test_style.py
-$PYTHON $NOSE -v tests/test_commandline.py
-$PYTHON $NOSE -v tests/test_scripts.py
--- jenkins_cgatpipelines_code.sh
+++ jenkins_cgatpipelines_code.sh
@@ -1,76 +1,22 @@
 #!/bin/bash -xe
 
-export DRMAA_LIBRARY_PATH=/ifs/apps/system/sge-6.2/lib/lx24-amd64/libdrmaa.so
-export SGE_ROOT=/ifs/apps/system/sge-6.2
-export SGE_CLUSTER_NAME=cgat
-export SGE_ARCH=lx24_x86
-export SGE_CELL=default
-
 if [ -z "$JENKINS_PYTHON_VERSION" ] ; then
     JENKINS_PYTHON_VERSION="2.7"
 fi
 
-if [[ $JENKINS_PYTHON_VERSION == "2.7" ]] ; then
-    PYTHON_MODULES="apps/python";
-    PYTHON_EXECUTABLE="python2.7";
-elif [[ $JENKINS_PYTHON_VERSION == "3.5" ]] ; then
-    PYTHON_MODULES="apps/python3";
-    PYTHON_EXECUTABLE="python3.5";
-else
+if [ $JENKINS_PYTHON_VERSION != "2.7" ] && [ $JENKINS_PYTHON_VERSION != "3.5" ] ; then
     echo "unsupported python version ${JENKINS_PYTHON_VERSION}"
     exit 1
 fi
 
-export MODULEPATH=/usr/share/Modules/modulefiles:/etc/modulefiles:/ifs/apps/modulefiles
-eval `modulecmd bash load ${PYTHON_MODULES} bio/all`
+# export env variables to installation script
+export JENKINS_PYTHON_VERSION
+export TEST_ALL=1
 
-# number of parallel jobs to run for testing
-NUM_JOBS=4
+# Required when testing install-CGAT-tools.sh on a branch called SLV-install-jenkins
+wget -O install-CGAT-pipelines.sh https://raw.githubusercontent.com/CGATOxford/CGATPipelines/SLV-install-jenkins/install-CGAT-tools.sh
+bash install-CGAT-pipelines.sh --jenkins
 
-# enter working directory. Needs to be on /ifs and mounted everywhere
-# /ifs/projects not possible as jenkins not part of projects group.
-WORKDIR=/ifs/mirror/jenkins/${JOB_NAME}
-
-if [ ! -d ${WORKDIR} ]; then
-    mkdir -p ${WORKDIR}
-fi
-
-cd ${WORKDIR}
-
-# setup virtual environment
-rm -rf test_python
-PYTHON_EXEC=`/usr/bin/which ${PYTHON_EXECUTABLE}`
-virtualenv --python=${PYTHON_EXEC} --system-site-packages test_python
-
-# activate virtual environment
-source test_python/bin/activate
-
-cd ${WORKSPACE}
-cd cgat && python setup.py develop
-
-# install CGATPipelines code and scripts. These need to be installed on
-# a shared location.
-cd ${WORKSPACE}
-cd CGATPipelines && python setup.py develop
-
-PYTHON=`which python`
-NOSE=`which nosetests`
-
-# some debugging information
-echo "----------------------------------------------"
-printenv
-which python
-python -c 'import numpy; print numpy.version.version'
-python -c 'import pysam; print pysam.__version__'
-python -c 'import matplotlib; print matplotlib.__version__'
-python -c 'from matplotlib.externals.six.moves.urllib.parse import quote'
-echo "python is $PYTHON"
-echo "nose is $NOSE"
-echo "----------------------------------------------"
-
-# run tests
-cd ${WORKSPACE}/CGATPipelines
-echo -e "restrict:\n    manifest:\n" > tests/_test_commandline.yaml
-# py.test -n ${NUM_JOBS} tests/test_*.py
-$PYTHON $NOSE -v tests/test_*.py
+# install and test cgat scripts
+# bash install-CGAT-tools.sh --jenkins
 
