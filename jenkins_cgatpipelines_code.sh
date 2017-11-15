6f541615c752ce3162cbba52aef08982d99ab20e
Sebastian Luna Valero <sebastian.luna.valero@gmail.com>
2017-11-15 09:49:05 +0000

    no need to specify Python version

--- jenkins_cgatpipelines_code.sh
+++ jenkins_cgatpipelines_code.sh
@@ -16,17 +16,7 @@ set -o pipefail
 # trace what gets executed
 set -o xtrace
 
-if [[ -z "$JENKINS_PYTHON_VERSION" ]] ; then
-    JENKINS_PYTHON_VERSION="2.7"
-fi
-
-if [[ "$JENKINS_PYTHON_VERSION" != "2.7" ]] && [[ "$JENKINS_PYTHON_VERSION" != "3.5" ]] ; then
-    echo "unsupported python version ${JENKINS_PYTHON_VERSION}"
-    exit 1
-fi
-
 # export env variables to installation script
-export JENKINS_PYTHON_VERSION
 export TEST_ALL=1
 
 # Required when testing install-CGAT-tools.sh on a branch called SLV-install-jenkins
